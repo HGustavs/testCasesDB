a29d8b5a61906edcf26a04562b6393507a90cb86
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2020-05-11 13:49:39 +0200

    Update testConnectionMySQL.php
    Styling and privilege assignment

--- testConnectionMySQL.php
+++ testConnectionMySQL.php
@@ -1,5 +1,29 @@
 <html>
-<body>
+<head>
+	<style>
+		body{
+				font-family:Arial Narrow;
+		}
+		
+		td{
+				margin:4px;
+				padding:4px;
+		}
+		
+		table{
+				border:2px solid #614875;
+				border-collapse: collapse;
+		}
+		
+		th{
+				background:#000;
+				color:white;
+		}
+		
+	</style>
+</head>
+
+<body style='font-family:Arial Narrow;'>
 
 <?php
  		echo "<h3>Connecting</h3>";
@@ -45,7 +69,7 @@
 	
 				echo "Connection / Table Initiation Test\n";		
 				$cnt=0;
-				echo "<table border='1'><tr><td>Table Name</td></tr>";
+				echo "<table><tr><th>Table Name</th></tr>";
 				foreach($pdo->query( 'show tables;' ) as $row){
 					echo "<tr>";
 					echo "<td>".$row[0]."</td>";
@@ -68,7 +92,7 @@
 				}		
 			
 				$cnt=0;
-				echo "<table border='1'>";			
+				echo "<table>";			
 				try{
 						foreach($pdo->query( 'SELECT * FROM admTest;' ) as $row){
 							if($cnt==0){
@@ -103,7 +127,7 @@
 				}		
 			
 				$cnt=0;
-				echo "<table border='1'>";			
+				echo "<table>";			
 				try{
 						foreach($pdo->query( 'SELECT * FROM mysql.user;' ) as $row){
 							if($cnt==0){
@@ -138,12 +162,40 @@
 
  		echo "<h3>Testing assignment of privileges</h3>";
 	
-		try{
-				$pdo->query( "GRANT ALL ON admTest TO 'malteknapp'@'localhost';");
+		try{		
+			$pdo->exec( "GRANT ALL PRIVILEGES ON * TO 'malteknapp'@'localhost';");
 		}catch(PDOException $e){
-				echo "Failed to assign privileges to user in database: " . $e->getMessage();
-		}			
+				echo "Failed to retrieve user accounts: " . $e->getMessage();
+		}
+			
 	    
+		echo "<table>";	
+		$cnt=0;
+		try{
+				foreach($pdo->query( "SHOW GRANTS FOR 'malteknapp'@'localhost';" ) as $row){
+					if($cnt==0){
+							echo "<tr>";
+							foreach($row as $colname => $col){
+									if(!is_numeric($colname)){
+											echo "<th>".$colname."</th>";
+									}
+							}
+							echo "</tr>";
+					}
+					echo "<tr>";
+					foreach($row as $colname => $col){
+							if(!is_numeric($colname)){
+									echo "<td>".$col."</td>";
+							}
+					}
+					echo "</tr>";
+					$cnt++;
+				}
+				echo "</table>";
+		}catch(PDOException $e){
+				echo "Failed to retrieve user accounts: " . $e->getMessage();
+		}
+	
 ?>
 </body>
 </html>
